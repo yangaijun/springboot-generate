d9c9c5997bf82ec1f4df85560624a4f5f3601f03
yangaijun <1316405700@qq.com>
2023-06-01 10:19:07 +0800

    mybatisplus配置修改

---
+++ src/main/java/com/light/common/utils/TokenUtil.java
@@ -0,0 +1,12 @@
+package com.light.common.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
+
+public class TokenUtil {
+
+}
