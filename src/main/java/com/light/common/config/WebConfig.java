5634738a484e6bb7c08c1d40087b5820b39d9e41
yangaijun <1316405700@qq.com>
2023-12-11 22:13:35 +0800

    日期格式化

--- src/main/java/com/light/common/config/WebConfig.java
+++ src/main/java/com/light/common/config/WebConfig.java
@@ -1,12 +1,22 @@
 package com.light.common.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.light.common.config.interceptor.AuthFilter;
-import com.light.common.config.interceptor.WebIntercepter;
+import com.light.common.config.interceptor.WebInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * web配置
@@ -14,13 +24,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @date 2017-08-23 15:48
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new WebIntercepter())
+        registry.addInterceptor(new WebInterceptor())
                 .addPathPatterns("/**");
-        super.addInterceptors(registry);
     }
 
     @Override
@@ -35,6 +45,22 @@ public class WebConfig extends WebMvcConfigurationSupport {
                 // 放行哪些原始请求头部信息
                 .allowedHeaders("*")
                 .exposedHeaders("*");
+    }
+
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+                .indentOutput(true)
+                .dateFormat(new SimpleDateFormat("yyyy-MM-dd"))
+                .simpleDateFormat(DATE_TIME_FORMAT)
+                .serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)))
+                .serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)))
+                .deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .modulesToInstall(new ParameterNamesModule());
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
     }
 
     @Bean
--- src/main/java/com/light/common/config/interceptor/WebInterceptor.java
+++ src/main/java/com/light/common/config/interceptor/WebInterceptor.java
@@ -3,11 +3,10 @@ package com.light.common.config.interceptor;
 import com.light.common.threadlocal.ThreadlocalManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-public class WebIntercepter implements HandlerInterceptor {
+public class WebInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return true;
